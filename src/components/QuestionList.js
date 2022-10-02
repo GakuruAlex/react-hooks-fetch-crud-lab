b0dd1ad5e7f85638eb1ec106da2caf1886f08bd3
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-10-02 22:14:27 +0300

    Assignment compoleted

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [question, setQuestion] = useState([]);
+
+
+
+  const url = "http://localhost:4000/questions";
+
+  // fetch method
+  useEffect(() => {
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => setQuestion(data));
+  }, []);
+
+  // patch method/ update
+  const handleUpdate = (id, correctIndex) => {
+    fetch(`${url}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updateAnswer) => {
+        const update = question.map((que) => {
+          if (que.id === updateAnswer.id) {
+            return update;
+          }
+          return que;
+        });
+        setQuestion(updateAnswer);
+      });
+  };
+  // console.log(handleUpdate)
+
+  // delete method
+    function handleDelete(id) {
+      fetch(`${url}${id}`, {
+        method: "DELETE",
+      })
+      .then(() => {
+        const updatedQuestions = question.filter((q) => q.id !== id);
+          setQuestion(updatedQuestions);
+        });
+    }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+
+        {question.map((aQuestion) => (
+
+          <QuestionItem key={aQuestion.id} quiz={aQuestion} handleUpdate={handleUpdate}
+          handleDelete={handleDelete}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
